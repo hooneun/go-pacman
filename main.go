3451c5edd22d635fa5916b52bb7cb4cc9bc90066
Hoon <kimhoon0316@gmail.com>
2020-10-21 17:08:43 +0900

    read file

--- main.go
+++ main.go
@@ -1,15 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var maze []string
+
+func loadMaze(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
+
+	return nil
+}
+
+func printScreen() {
+	for _, line := range maze {
+		fmt.Println(line)
+	}
+}
 
 func main() {
 	// initialize game
 
 	// load resources
+	err := loadMaze("maze01.txt")
+	if err != nil {
+		log.Println("failed to load maze:", err)
+		return
+	}
 
 	// game loop
 	for {
 		// update screen
+		printScreen()
 
 		//process input
 
@@ -20,7 +55,7 @@ func main() {
 		//check game over
 
 		// Temp: break infinite loop
-		fmt.Println("Hello, Pac Go!")
+		// fmt.Println("Hello, Pac Go!")
 		break
 
 		//repeat
